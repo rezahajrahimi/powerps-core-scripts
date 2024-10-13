3465260872965f7aca58c131a58c3928c1246ce8
Reza HajRahimi <rezahajrahimi@gmail.com>
2024-10-13 14:46:17 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,61 +1,115 @@
 #!/bin/bash
+
 # Color codes
 GREEN='\033[0;32m'
 CYAN='\033[0;36m'
 YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
-# Set error handling
-set -e
+# Pretty title
+echo -e "${CYAN}==============================${NC}"
+echo -e "${YELLOW}  Setting up or Updating your Laravel Project${NC}"
+echo -e "${CYAN}==============================${NC}"
 
-# Define variables with quotes
-APP_NAME="powerps-core"
-DB_PASSWORD=$(openssl rand -base64 32)
-REPO_URL="https://github.com/rezahajrahimi/powerps-core.git"
+# Check if the project directory exists
+if [ -d "/var/www/html/laravel-app" ]; then
+    # If it exists, update the repository
+    echo -e "${GREEN}Updating the Laravel project repository...${NC}"
+    cd /var/www/html/laravel-app
+    git pull origin main
+    
+    # Install Composer dependencies
+    echo -e "${GREEN}Installing Composer dependencies...${NC}"
+    composer install
+else
+    # Otherwise, proceed with the full setup
+    echo -e "${GREEN}Updating package lists...${NC}"
+    sudo apt-get update
+    echo -e "${GREEN}Installing necessary packages...${NC}"
+    sudo apt-get install -y apache2 mysql-server php8.3 php8.3-mysql libapache2-mod-php8.3 php8.3-cli php8.3-zip php8.3-xml php8.3-mbstring php8.3-curl composer unzip git expect
 
-# Define configurable paths
-INSTALL_DIR="/var/www/html/${APP_NAME}"
-HTML_DIR="${INSTALL_DIR}/html"
-COMPOSER_DIR="${INSTALL_DIR}/composer"
-LOG_DIR="${INSTALL_DIR}/logs"
+    # Secure MySQL Installation using expect
+    echo -e "${GREEN}Securing MySQL Installation...${NC}"
+    SECURE_MYSQL=$(expect -c "
+    set timeout 10
+    spawn sudo mysql_secure_installation
+    expect \"VALIDATE PASSWORD COMPONENT can be used to test passwords\"
+    send \"n\r\"
+    expect \"New password:\"
+    send \"yourpassword\r\"
+    expect \"Re-enter new password:\"
+    send \"yourpassword\r\"
+    expect \"Do you wish to continue with the password provided?\"
+    send \"y\r\"
+    expect \"Remove anonymous users?\"
+    send \"y\r\"
+    expect \"Disallow root login remotely?\"
+    send \"y\r\"
+    expect \"Remove test database and access to it?\"
+    send \"y\r\"
+    expect \"Reload privilege tables now?\"
+    send \"y\r\"
+    expect eof
+    ")
 
-# Install PHP 8.3 with necessary extensions
-sudo add-apt-repository ppa:ondrej/php
-sudo apt-get update
-sudo apt-get install -y php8.3 php8.3-fpm php8.3-mysql php8.3-curl php8.3-gd php8.3-mbstring php8.3-xml php8.3-zip php8.3-bcmath php8.3-opcache
+    echo "$SECURE_MYSQL"
 
-# Copy bolt.so extension to PHP extensions directory
-sudo cp "${INSTALL_DIR}/bolt.so" /usr/lib/php/20230831/
+    # Create MySQL database and user
+    DB_NAME='laravel_db'
+    DB_USER='laravel_user'
+    DB_PASS='password'
+    echo -e "${GREEN}Creating MySQL database and user...${NC}"
+    sudo mysql -e "CREATE DATABASE ${DB_NAME};"
+    sudo mysql -e "CREATE USER '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASS}';"
+    sudo mysql -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'localhost';"
+    sudo mysql -e "FLUSH PRIVILEGES;"
 
-# Get the location of the php.ini file
-PHP_INI_FILE=$(php --ini | grep "Loaded Configuration File" | cut -d ":" -f 2- | tr -d " ")
+    # Clone the Laravel project repository
+    echo -e "${GREEN}Cloning Laravel project repository...${NC}"
+    git clone https://github.com/rezahajrahimi/powerps-core /var/www/html/laravel-app
+    cd /var/www/html/laravel-app
 
-# Add bolt.so extension to main php.ini
-sudo echo "extension=bolt.so" | sudo tee -a "${PHP_INI_FILE}"
+    # Install Composer dependencies
+    echo -e "${GREEN}Installing Composer dependencies...${NC}"
+    composer install
 
-# Install other dependencies
-sudo apt-get install -y git composer apache2 mysql-server
+    # Prompt user for .env variables only if .env does not exist
+    if [ ! -f ".env" ]; then
+        echo -e "${CYAN}Please enter the following environment variables for your Laravel project:${NC}"
+        read -p "App Name: " APP_NAME
+        read -p "Environment (local/production): " APP_ENV
+        read -p "App Debug (true/false): " APP_DEBUG
+        read -p "App URL: " APP_URL
 
-# Create install directory
-if [ -d "${INSTALL_DIR}" ]; then
-  echo "Directory ${INSTALL_DIR} already exists. Deleting it..."
-  sudo rm -rf "${INSTALL_DIR}"
-fi
+        read -p "NOWPayments API Key: " NOWPAYMENTS_API_KEY
+        read -p "Telegram Bot Token: " TELEGRAM_BOT_TOKEN
+        read -p "Telegram Admin ID: " TELEGRAM_ADMIN_ID
+        read -p "Zarinpal Merchant ID: " ZARINPAL_MERCHANT_ID
+        read -p "URL for Front-End Project: " FRONT_URL
 
-sudo mkdir -p "${INSTALL_DIR}"
-sudo chown -R "$USER:$USER" "${INSTALL_DIR}"
-if [ ! -f "/var/www/html/powerps-core/.env" ]; then
-  touch "/var/www/html/powerps-core/.env"
-fi
+        # Set up Laravel environment
+        echo -e "${GREEN}Setting up Laravel environment...${NC}"
+        cp .env.example .env
 
-# Clone repository
-git clone "${REPO_URL}" "${INSTALL_DIR}"
+        # Configure .env file with user input
+        echo -e "${GREEN}Configuring .env file...${NC}"
+        sed -i "s/^APP_NAME=.*/APP_NAME=${APP_NAME}/" .env
+        sed -i "s/^APP_ENV=.*/APP_ENV=${APP_ENV}/" .env
+        sed -i "s/^APP_DEBUG=.*/APP_DEBUG=${APP_DEBUG}/" .env
+        sed -i "s|^APP_URL=.*|APP_URL=${APP_URL}|" .env
 
-# Install composer dependencies
-cd "${INSTALL_DIR}"
-composer install
+        sed -i "s/^DB_DATABASE=.*/DB_DATABASE=${DB_NAME}/" .env
+        sed -i "s/^DB_USERNAME=.*/DB_USERNAME=${DB_USER}/" .env
+        sed -i "s/^DB_PASSWORD=.*/DB_PASSWORD=${DB_PASS}/" .env
 
-
+        echo "NOWPAYMENTS_API_KEY=${NOWPAYMENTS_API_KEY}" >> .env
+        echo "TELEGRAM_BOT_TOKEN=${TELEGRAM_BOT_TOKEN}" >> .env
+        echo "TELEGRAM_ADMIN_ID=${TELEGRAM_ADMIN_ID}" >> .env
+        echo "ZARINPAL_MERCHANT_ID=${ZARINPAL_MERCHANT_ID}" >> .env
+        echo "FRONT_URL=${FRONT_URL}" >> .env
+    else
+        echo -e "${GREEN}.env file exists, skipping environment configuration...${NC}"
+    fi
 
     # Generate app key
     echo -e "${GREEN}Generating app key...${NC}"
@@ -65,13 +119,6 @@ composer install
     echo -e "${GREEN}Running migrations...${NC}"
     php artisan migrate
 
-    # Check if database exists
-if mysql -u root -p${DB_PASSWORD} -e "SHOW DATABASES LIKE '${APP_NAME}';" &> /dev/null; then
-  echo "Error: Database already exists. Please choose a different name for your app."
-  exit 1
-fi
-
-    
     # Install PHPMyAdmin
     echo -e "${GREEN}Installing PHPMyAdmin...${NC}"
     cd /var/www/html
@@ -79,6 +126,17 @@ fi
     unzip phpMyAdmin-latest-all-languages.zip
     mv phpMyAdmin-*-all-languages phpmyadmin
     rm phpMyAdmin-latest-all-languages.zip
+
+    # Copy bolt.so extension to PHP extensions directory
+    echo -e "${GREEN}Copying bolt.so extension...${NC}"
+    sudo cp "${INSTALL_DIR}/bolt.so" /usr/lib/php/20230831/
+
+    # Get the location of the php.ini file
+    PHP_INI_FILE=$(php --ini | grep "Loaded Configuration File" | cut -d ":" -f 2- | tr -d " ")
+
+    # Add bolt.so extension to main php.ini
+    echo -e "${GREEN}Adding bolt.so extension to php.ini...${NC}"
+    echo "extension=bolt.so" | sudo tee -a "${PHP_INI_FILE}"
 
     # Restart Apache to apply changes
     echo -e "${GREEN}Restarting Apache to apply changes...${NC}"
@@ -96,13 +154,12 @@ fi
         AllowOverride All
         Require all granted
     </Directory>
-
-    ErrorLog \${APACHE_LOG_DIR}/error.log
+        ErrorLog \${APACHE_LOG_DIR}/error.log
     CustomLog \${APACHE_LOG_DIR}/access.log combined
 </VirtualHost>
 EOF
 
-# Enable new site and rewrite module
+    # Enable new site and rewrite module
     echo -e "${GREEN}Enabling new site and rewrite module...${NC}"
     sudo a2ensite laravel
     sudo a2enmod rewrite
@@ -124,29 +181,9 @@ EOF
     php artisan serve &
 fi
 
-# Create logs directory
-sudo mkdir -p "${LOG_DIR}"
-sudo chown -R "$USER:$USER" "${LOG_DIR}"
-
-# Create html directory
-sudo mkdir -p "${HTML_DIR}"
-sudo chown -R "$USER:$USER" "${HTML_DIR}"
-
-# Create symbolic link
-sudo ln -s "${INSTALL_DIR}/public" "${HTML_DIR}"
+echo -e "${CYAN}==============================${NC}"
+echo -e "${YELLOW}  Setup Complete!${NC}"
+echo -e "${CYAN}==============================${NC}"
+echo -e "${GREEN}Laravel project with MySQL, PHPMyAdmin setup, and scheduled command complete!${NC}"
 
 
-# Create database and user
-mysql -u root -p${DB_PASSWORD} -e "CREATE DATABASE ${APP_NAME};"
-mysql -u root -p${DB_PASSWORD} -e "GRANT ALL ON ${APP_NAME}.* TO ${APP_NAME}@localhost IDENTIFIED BY '${DB_PASSWORD}';"
-mysql -u root -p${DB_PASSWORD} -e "FLUSH PRIVILEGES;"
-
-# Set database password
-sed -i "s/DB_PASSWORD=.*/DB_PASSWORD=${DB_PASSWORD}/" "${INSTALL_DIR}/.env"
-
-# Log success
-echo "Installation complete!"
-
-# Display instructions
-echo "Please visit http://${APP_NAME}.localhost in your web browser to access the application."
-echo "Please note that you may need to configure your hosts file to access the application."
